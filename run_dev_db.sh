f6795fe8cbf9bdd15925e7e090149f564fe8b823
Lukas Thum <you@example.com>
2023-05-04 00:21:39 -0400

    up and running

--- run_dev_db.sh
+++ run_dev_db.sh
@@ -1,7 +1,9 @@
+# !/bin/bash
 docker run -d \
   --name arcane_depths_db \
   -e POSTGRES_USER=postgres \
   -e POSTGRES_PASSWORD=postgres \
   -e POSTGRES_DB=arcane_depths_dev \
+  -v "$(pwd)/tmp/data:/var/lib/postgresql/data" \
   -p 5432:5432 \
   postgres:latest
